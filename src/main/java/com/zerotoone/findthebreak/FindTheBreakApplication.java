5811a2a41d9dd2d01b6fc987d6f4c8598eaef8c6
JAE <jaegyumjaegyum@mgail.com>
2022-07-16 15:38:37 +0900

    프로젝트 첫 업로드

---
+++ src/main/java/com/zerotoone/findthebreak/FindTheBreakApplication.java
@@ -0,0 +1,13 @@
+package com.zerotoone.findthebreak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindTheBreakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindTheBreakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerotoone/findthebreak/FindTheBreakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerotoone.findthebreak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindTheBreakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
